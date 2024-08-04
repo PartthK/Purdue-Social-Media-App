940d0819a66e7522b80102ba85b1c8758878f3e2
PartthK <partthkul@gmail.com>
2024-08-04 23:16:15 +0530

    made the add friend option

--- lib/profile_screen.dart
+++ lib/profile_screen.dart
@@ -19,66 +19,6 @@ class ProfileScreen extends StatefulWidget {
 class _ProfileScreenState extends State<ProfileScreen> {
   final ImagePicker _picker = ImagePicker();
   String? _profileImageUrl;
-  String _friendRequestStatus = 'Send Friend Request';
-  List<String> _friends = [];
-
-  @override
-  void initState() {
-    super.initState();
-    _checkFriendRequestStatus();
-    _fetchFriends();
-  }
-
-  Future<void> _checkFriendRequestStatus() async {
-    final currentUser = FirebaseAuth.instance.currentUser;
-    if (currentUser == null) return;
-
-    final sentRequestSnapshot = await FirebaseFirestore.instance
-        .collection('users')
-        .doc(widget.userId)
-        .collection('friendRequests')
-        .where('from', isEqualTo: currentUser.email)
-        .get();
-
-    final receivedRequestSnapshot = await FirebaseFirestore.instance
-        .collection('users')
-        .doc(currentUser.email)
-        .collection('friendRequests')
-        .where('from', isEqualTo: widget.userId)
-        .get();
-
-    final friendsSnapshot = await FirebaseFirestore.instance
-        .collection('users')
-        .doc(currentUser.email)
-        .collection('friends')
-        .doc(widget.userId)
-        .get();
-
-    setState(() {
-      if (friendsSnapshot.exists) {
-        _friendRequestStatus = 'Friends';
-      } else if (receivedRequestSnapshot.docs.isNotEmpty) {
-        _friendRequestStatus = 'Accept Friend Request';
-      } else if (sentRequestSnapshot.docs.isNotEmpty) {
-        _friendRequestStatus = 'Request Sent';
-      } else {
-        _friendRequestStatus = 'Send Friend Request';
-      }
-    });
-  }
-
-  Future<void> _fetchFriends() async {
-    FirebaseFirestore.instance
-        .collection('users')
-        .doc(widget.userId)
-        .collection('friends')
-        .snapshots()
-        .listen((snapshot) {
-      setState(() {
-        _friends = snapshot.docs.map((doc) => doc.id).toList();
-      });
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -122,10 +62,10 @@ class _ProfileScreenState extends State<ProfileScreen> {
             );
           }
 
-          final data = snapshot.data!.data() as Map<String, dynamic>?;
-          final name = data?['name'] ?? 'N/A';
-          final tags = List<String>.from(data?['tags'] ?? []);
-          _profileImageUrl = data?['profileImageUrl'] ?? null;
+          final data = snapshot.data!.data() as Map<String, dynamic>;
+          final name = data['name'] ?? 'N/A';
+          final tags = List<String>.from(data['tags'] ?? []);
+          _profileImageUrl = data['profileImageUrl'] ?? null;
 
           return Padding(
             padding: EdgeInsets.all(16.0),
@@ -160,48 +100,13 @@ class _ProfileScreenState extends State<ProfileScreen> {
                   style: GoogleFonts.montserrat(fontSize: 18.0),
                 )),
                 SizedBox(height: 16.0),
-                if (currentUser?.email != widget.userId && _friendRequestStatus != 'Friends')
+                if (currentUser?.email != widget.userId)
                   ElevatedButton(
-                    onPressed: _friendRequestStatus == 'Send Friend Request'
-                        ? () => sendFriendRequest(widget.userId)
-                        : null,
-                    child: Text(_friendRequestStatus),
-                  ),
-                SizedBox(height: 16.0),
-                Text(
-                  'Friends:',
-                  style: GoogleFonts.montserrat(fontSize: 24.0, fontWeight: FontWeight.bold),
-                ),
-                SizedBox(height: 8.0),
-                _friends.isEmpty
-                    ? Text(
-                  'No friends to display',
-                  style: GoogleFonts.montserrat(fontSize: 18.0),
-                )
-                    : Expanded(
-                  child: ListView.builder(
-                    itemCount: _friends.length,
-                    itemBuilder: (context, index) {
-                      return FutureBuilder<DocumentSnapshot>(
-                        future: FirebaseFirestore.instance.collection('users').doc(_friends[index]).get(),
-                        builder: (context, snapshot) {
-                          if (!snapshot.hasData || !snapshot.data!.exists) {
-                            return Container();
-                          }
-
-                          final friendData = snapshot.data!.data() as Map<String, dynamic>?;
-                          final friendName = friendData?['name'] ?? 'Unknown';
-                          final friendEmail = friendData?['email'] ?? 'Unknown';
-
-                          return ListTile(
-                            title: Text(friendName),
-                            subtitle: Text(friendEmail),
-                          );
-                        },
-                      );
+                    onPressed: () {
+                      sendFriendRequest(widget.userId);
                     },
+                    child: Text('Send Friend Request'),
                   ),
-                ),
               ],
             ),
           );
@@ -236,10 +141,6 @@ class _ProfileScreenState extends State<ProfileScreen> {
       await FirebaseFirestore.instance.collection('users').doc(friendId).collection('friendRequests').add({
         'from': currentUser.email,
         'timestamp': FieldValue.serverTimestamp(),
-      });
-
-      setState(() {
-        _friendRequestStatus = 'Request Sent';
       });
 
       // Send notification
