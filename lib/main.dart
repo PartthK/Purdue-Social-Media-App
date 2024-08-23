27c7c1eaa8fab0cb15536fd5f291b220d34be20d
baliutkarsh2 <baliutkarsh2@gmail.com>
2024-08-23 19:43:03 -0400

    eventTags

--- lib/main.dart
+++ lib/main.dart
@@ -32,6 +32,7 @@ class MyApp extends StatelessWidget {
             routes: {
               '/home': (context) => HomeScreen(),
               '/auth': (context) => AuthScreen(),
+
             },
             onGenerateRoute: (settings) {
               if (settings.name == '/interests') {
