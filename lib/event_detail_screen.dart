e94e349f78282d4c393498ee6c94b309a4ac97a5
PartthK <109812828+PartthK@users.noreply.github.com>
2024-08-23 20:22:51 -0400

    Update event_detail_screen.dart

--- lib/event_detail_screen.dart
+++ lib/event_detail_screen.dart
@@ -1,8 +1,9 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'event_model.dart';
+import 'package:firebase_auth/firebase_auth.dart'; // Assuming FirebaseAuth is used for authentication
 
 class EventDetailScreen extends StatefulWidget {
   final Event event;
@@ -47,79 +48,75 @@ class _EventDetailScreenState extends State<EventDetailScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return DraggableScrollableSheet(
-      initialChildSize: 0.9,
-      minChildSize: 0.5,
-      maxChildSize: 0.95,
-      builder: (_, controller) => Container(
-        decoration: BoxDecoration(
-          color: Theme.of(context).canvasColor,
-          borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
-        ),
-        child: ListView(
-          controller: controller,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.event.title),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Container(
-              height: 200,
-              decoration: BoxDecoration(
-                color: Colors.blue,
-                borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
-              ),
-              child: Center(
-                child: Text(
-                  widget.event.title,
-                  style: TextStyle(fontSize: 24, color: Colors.white, fontWeight: FontWeight.bold),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+            Text('Title: ${widget.event.title}'),
+            Text('Created By: ${widget.event.createdBy}'),
+            Text('RSVP Count: $_currentRSVPCount'),
+            Text('Date: ${widget.event.date}'),
+            Text('Description: ${widget.event.description}'),
+            RichText(
+              text: TextSpan(
+                text: 'Location: ',
+                style: TextStyle(color: Colors.black),
                 children: [
-                  _buildInfoRow(Icons.calendar_today, 'Date', DateFormat('MMM d, y - h:mm a').format(widget.event.date)),
-                  _buildInfoRow(Icons.location_on, 'Location', widget.event.location),
-                  _buildInfoRow(Icons.person, 'Created by', widget.event.createdBy),
-                  _buildInfoRow(Icons.group, 'RSVP Count', '$_currentRSVPCount'),
-                  SizedBox(height: 16),
-                  Text('Description', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-                  SizedBox(height: 8),
-                  Text(widget.event.description),
-                  SizedBox(height: 16),
-                  if (!_hasRSVPed)
-                    ElevatedButton(
-                      onPressed: () => _incrementRSVPCount(widget.event.documentId),
-                      child: Text('RSVP'),
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.blue,
-                        foregroundColor: Colors.white,
-                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
-                      ),
-                    )
-                  else
-                    Text(
-                      'You have already RSVPed to this event.',
-                      style: TextStyle(color: Colors.green, fontWeight: FontWeight.bold),
-                    ),
+                  TextSpan(
+                    text: widget.event.location,
+                    style: TextStyle(color: Colors.blue, decoration: TextDecoration.underline),
+                    recognizer: TapGestureRecognizer()
+                      ..onTap = () async {
+                        final Uri url = Uri.parse("https://www.google.com/maps/search/?api=1&query=${widget.event.location}");
+                        if (await canLaunchUrl(url)) {
+                          await launchUrl(url);
+                        } else {
+                          throw 'Could not launch $url';
+                        }
+                      },
+                  ),
                 ],
               ),
             ),
+            Text('Username: ${widget.event.username}'),
+            if (widget.event.locationMap.isNotEmpty)
+              GestureDetector(
+                onTap: () async {
+                  final url = Uri.parse(widget.event.locationMap);
+                  if (await canLaunchUrl(url)) {
+                    await launchUrl(url);
+                  } else {
+                    throw 'Could not launch $url';
+                  }
+                },
+                child: Text(
+                  'Open Location in Google Maps',
+                  style: TextStyle(color: Colors.blue),
+                ),
+              ),
+            SizedBox(height: 20),
+            if (!_hasRSVPed)
+              ElevatedButton(
+                onPressed: () => _incrementRSVPCount(widget.event.documentId),
+                child: Text('RSVP'),
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  backgroundColor: Colors.blue,
+                  textStyle: TextStyle(fontSize: 16),
+                ),
+              )
+            else
+              Text(
+                'You have already RSVPed to this event.',
+                style: TextStyle(color: Colors.red, fontSize: 16),
+              ),
           ],
         ),
-      ),
-    );
-  }
-
-  Widget _buildInfoRow(IconData icon, String label, String value) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8.0),
-      child: Row(
-        children: [
-          Icon(icon, size: 20, color: Colors.blue),
-          SizedBox(width: 8),
-          Text('$label: ', style: TextStyle(fontWeight: FontWeight.bold)),
-          Expanded(child: Text(value)),
-        ],
       ),
     );
   }
@@ -150,13 +147,4 @@ class _EventDetailScreenState extends State<EventDetailScreen> {
       });
     }
   }
-}
-
-void showEventDetails(BuildContext context, Event event) {
-  showModalBottomSheet(
-    context: context,
-    isScrollControlled: true,
-    backgroundColor: Colors.transparent,
-    builder: (context) => EventDetailScreen(event: event),
-  );
 }
